86366d4517191ef172a73197882fbf2c4b329c9b
kadala <kadala@kadala-Inspiron-1545.(none)>
2014-01-31 12:43:21 +0100

    fix config

--- src/Application/Sylius/SettingsBundle/Controller/ParameterAdminController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Application\Sylius\SettingsBundle\Controller;
-
-use Sonata\AdminBundle\Controller\CRUDController as Controller;
-
-class ParameterAdminController extends Controller
-{
-}
